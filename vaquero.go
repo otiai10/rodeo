4bac08c1f72522896eb519cecbab6ced6ce00237
otiai10 <otiai10@gmail.com>
2014-05-01 13:47:39 +0900

    Add skel of method `TheVaquero`

--- vaquero.go
+++ vaquero.go
@@ -5,5 +5,7 @@ import "github.com/robfig/config"
 type Vaquero struct {
 }
 
-func TheVaquero(conf *config.Config) {
+func TheVaquero(conf *config.Config) (v *Vaquero, e error) {
+	v = &Vaquero{}
+	return
 }
--- vaquero_test.go
+++ vaquero_test.go
@@ -1,6 +1,13 @@
 package rodeo_test
 
+import . "github.com/otiai10/rodeo"
+
 import "testing"
+import "fmt"
+import "github.com/robfig/config"
 
 func TestVaquero(t *testing.T) {
+	conf, _ := config.ReadDefault("sample.conf")
+	vaquero, _ := TheVaquero(conf)
+	fmt.Println(vaquero)
 }
