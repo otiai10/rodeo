3f6b592a4e485ebf8544daa5473113d2a3d4f4a2
otiai10 <otiai10@gmail.com>
2014-05-01 13:38:12 +0900

    Add testing skel

---
+++ vaquero.go
@@ -0,0 +1,9 @@
+package rodeo
+
+import "github.com/robfig/config"
+
+type Vaquero struct {
+}
+
+func TheVaquero(conf *config.Config) {
+}
---
+++ vaquero_test.go
@@ -0,0 +1,6 @@
+package rodeo_test
+
+import "testing"
+
+func TestVaquero(t *testing.T) {
+}
