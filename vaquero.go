6967d28bb216377528a63b51cc462ee5fe663b01
otiai10 <otiai10@gmail.com>
2014-05-01 14:24:58 +0900

    Vaquero: get conf

--- vaquero.go
+++ vaquero.go
@@ -3,9 +3,30 @@ package rodeo
 import "github.com/robfig/config"
 
 type Vaquero struct {
+	Conf C // should not be exported?
 }
 
-func TheVaquero(conf *config.Config) (v *Vaquero, e error) {
-	v = &Vaquero{}
+type C struct {
+	Host string
+	Port string
+}
+
+func TheVaquero(conf *config.Config, args ...string) (v *Vaquero, e error) {
+	var c C
+	c, e = ensureConf(conf, args)
+	v = &Vaquero{
+		c,
+	}
+	return
+}
+
+func ensureConf(conf *config.Config, args []string) (c C, e error) {
+	var host, port string
+	host, e = conf.String("test", "host")
+	port, e = conf.String("test", "port")
+	c = C{
+		host,
+		port,
+	}
 	return
 }
--- vaquero_test.go
+++ vaquero_test.go
@@ -7,7 +7,7 @@ import "fmt"
 import "github.com/robfig/config"
 
 func TestVaquero(t *testing.T) {
-	conf, _ := config.ReadDefault("sample.conf")
-	vaquero, _ := TheVaquero(conf)
-	fmt.Println(vaquero)
+	conf, _ := config.ReadDefault("test.conf")
+	vaquero, e := TheVaquero(conf)
+	fmt.Println(vaquero, e)
 }
