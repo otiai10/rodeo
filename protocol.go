0e498dad38790c19f158a840194ce8f0775d18fe
otiai10 <otiai10@gmail.com>
2014-05-03 16:45:50 +0900

    Protocol should have connection of TCP

--- protocol.go
+++ protocol.go
@@ -1,10 +1,12 @@
 package rodeo
 
+import "net"
+
 // すべてのKVSに対応する
 // 文字長、文字列の整形変換などする
 type Protocol interface {
 	Request(args ...interface{}) Protocol
-	Execute() Protocol
+	Execute(conn net.Conn) Protocol
 	ToResult() Result
 }
 type Result struct{}
--- redis.go
+++ redis.go
@@ -1,5 +1,7 @@
 package rodeo
 
+import "net"
+
 // 相手がredisであることを知っている
 // redis特有の文字列整形を知っている
 // redisのロジックはすべてここに閉じ込める
@@ -8,7 +10,7 @@ type RedisProtocol struct{}
 func (p *RedisProtocol) Request(args ...interface{}) Protocol {
 	return p
 }
-func (p *RedisProtocol) Execute() Protocol {
+func (p *RedisProtocol) Execute(conn net.Conn) Protocol {
 	return p
 }
 func (p *RedisProtocol) ToResult() Result {
